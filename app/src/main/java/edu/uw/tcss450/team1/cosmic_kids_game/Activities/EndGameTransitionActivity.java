ce38520688fbe5d0430e813c75a3a970bfa8b3dd
Brandon W Chambers <bdubchambers@hotmail.com>
2016-05-30 15:34:45 -0700

    Adding the EndGameTransitionActivity that will serve as a transition between ending games and returning to the MainScreen.  Will show the user's score for the game and the name of the game just completed.

---
+++ app/src/main/java/edu/uw/tcss450/team1/cosmic_kids_game/Activities/EndGameTransitionActivity.java
@@ -0,0 +1,16 @@
+package edu.uw.tcss450.team1.cosmic_kids_game.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.uw.tcss450.team1.cosmic_kids_game.R;
+
+
+public class EndGameTransitionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game_transition);
+    }
+}
