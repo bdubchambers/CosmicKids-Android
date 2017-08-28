16edb2fdd4494ff80199f0b16bd0529cfde89974
bdubchambers <bdubchambers@hotmailcom>
2017-08-27 21:56:31 -0700

    Tabbed php files

--- app/src/main/phpscripts/remote_config.inc.php
+++ app/src/main/phpscripts/remote_config.inc.php
@@ -1,40 +1,40 @@
-
 <?php
-   #Connection Credentials
-   $username = "_450btm1";
-   $password = "crajWoaf";
-   $host = "cssgate.insttech.washington.edu";
-   $dbname = "_450btm1";
 
-   #Set character encoding scheme
-   $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-   #Attempt opening a connection to database with the PDO library interface.
-   try {
-     $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",
-       $username, $password, $options);
-   } catch (PDOException $ex) {
-     die("Failed to connect to the database: " . $ex->getMessage());
-   }
+	#Connection Credentials
+	$username = "_450btm1";
+	$password = "crajWoaf";
+	$host = "cssgate.insttech.washington.edu";
+	$dbname = "_450btm1";
 
-   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	#Set character encoding scheme
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+	#Attempt opening a connection to database with the PDO library interface.
+	try {
+		$db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",
+		$username, $password, $options);
+	} catch (PDOException $ex) {
+		die("Failed to connect to the database: " . $ex->getMessage());
+	}
 
-   if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-     function undo_magic_quotes_gpc(&$array) {
-       foreach ($array as &$value) {
-         if(is_array($value))
-           undo_magic_quotes_gpc($value);
-         else
-           $value = stripslashes($value);
-       }
-     }
-   undo_magic_quotes_gpc($_POST);
-   undo_magic_quotes_gpc($_GET);
-   undo_magic_quotes_gpc($_COOKIE);
-   }
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-   #Inform webbrowser that we use utf8
-   header('Content-Type: text/html; charset=utf-8');
+	if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+		function undo_magic_quotes_gpc(&$array) {
+			foreach ($array as &$value) {
+				if(is_array($value))
+					undo_magic_quotes_gpc($value);
+				else
+					$value = stripslashes($value);
+			}
+		}
+		undo_magic_quotes_gpc($_POST);
+		undo_magic_quotes_gpc($_GET);
+		undo_magic_quotes_gpc($_COOKIE);
+	}
 
-   session_start();
-  ?>
+	#Inform webbrowser that we use utf8
+	header('Content-Type: text/html; charset=utf-8');
+
+	session_start();
+ ?>
--- app/src/main/phpscripts/scores.php
+++ app/src/main/phpscripts/scores.php
@@ -1,42 +1,42 @@
 <?php
-  require 'config.inc.php';
+	require 'config.inc.php';
 
-  $query = "SELECT * FROM scores";
+	$query = "SELECT * FROM scores";
 
-  #execute query
-  try {
-    $stmt   = $db->prepare($query);
-    $result = $stmt->execute($query_params);
-  } catch (PDOException $ex) {
-    $response["success"] = 0;
-    $response["message"] = "Database Error (1). Try again.";
-    die(json_encode($response));
-  }
+	#execute query
+	try {
+		$stmt   = $db->prepare($query);
+		$result = $stmt->execute($query_params);
+	} catch (PDOException $ex) {
+		$response["success"] = 0;
+		$response["message"] = "Database Error (1). Try again.";
+		die(json_encode($response));
+	}
 
-  #Retrieve all existing score rows from db
-  $rows = $stmt->fetchAll();
+	#Retrieve all existing score rows from db
+	$rows = $stmt->fetchAll();
 
-  if($rows) {
-    $response["success"] = 1;
-    $response["message"] = "Score Available!";
-    $response["scores"]   = array();
+	if($rows) {
+		$response["success"] = 1;
+		$response["message"] = "Score Available!";
+		$response["scores"]   = array();
 
-    foreach ($rows as $row) {
-      $post = array();
-      $post["entry_id"] = $row["entry_id"];
-      $post["username"] = $row["username"];
-      $post["gamename"] = $row["gamename"];
-      $post["score"] = $row["score"];
-      #add to json data
-      array_push($response["scores"], $post);
-    }
+		foreach ($rows as $row) {
+			$post = array();
+			$post["entry_id"] = $row["entry_id"];
+			$post["username"] = $row["username"];
+			$post["gamename"] = $row["gamename"];
+			$post["score"] = $row["score"];
+			#add to json data
+			array_push($response["scores"], $post);
+		}
 
-    echo json_encode($response);
-  }
-  else {
-    $response["success"] = 0;
-    $response["message"] = "No scores available.";
-    die(json_encode($response));
-  }
+		echo json_encode($response);
+	}
+	else {
+		$response["success"] = 0;
+		$response["message"] = "No scores available.";
+		die(json_encode($response));
+	}
 
 ?>
